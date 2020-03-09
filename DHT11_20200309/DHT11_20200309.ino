65db9ad7f090e3010272ae9c3039a036cdab5800
minexo79 <minexo79@gmail.com>
2020-03-09 21:11:22 +0800

    upload ino files.

---
+++ DHT11_20200309/DHT11_20200309.ino
@@ -0,0 +1,50 @@
+#include <LiquidCrystal_I2C.h> // I2C include
+#include <Wire.h> // Arduino include
+#include <SimpleDHT.h> // DHT include
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int pinDHT = 2;
+SimpleDHT11 dht11;
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    lcd.begin();
+    Serial.println("BOOTING...OK!");
+}
+
+int i2c_error(int code) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DHT11 failed!");
+    lcd.setCursor(0, 1);
+    lcd.print(code);
+    delay(500);  
+}
+
+int i2c_display(byte temp,byte humi) {
+    lcd.setCursor(0, 0);
+    lcd.print("Humidity: ");
+    lcd.print((int)humi);
+    lcd.print("%");
+    lcd.setCursor(0, 1);
+    lcd.print("Temp: ");
+    lcd.print((int)temp);
+    lcd.print("C");    
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    byte temperature = 0;
+    byte humidity = 0;
+    int err = SimpleDHTErrSuccess;
+    // Check DHT11 error
+    if ((err = dht11.read(pinDHT,&temperature,&humidity,NULL))!= SimpleDHTErrSuccess) {
+        // Send infomation to LCD.
+        i2c_error(err);
+        return;
+    }
+    // Send infomation to LCD.
+    i2c_display(temperature,humidity);
+    delay(1500);  
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+![size](https://img.shields.io/github/repo-size/minexo79/old_things)
+![pip support](https://img.shields.io/pypi/pyversions/discord.py)
+![pip version](https://img.shields.io/pypi/v/discord.py?color=red) 
+![Arduino](https://img.shields.io/badge/Arduino-DHT11-yellowgreen)  
+![commit](https://img.shields.io/github/last-commit/minexo79/old_things)
+
+這裡存放之前的專案：
+---
+
++ Hakkaman
+    + `客家人DC BOT (Python)`
++ py-monbot
+    + `芒果DC BOT (Python)`
++ vmhelper
+    + `VM小助手DC BOT (Python)`  
++ Bh-crazynews
+    + `狂新聞DC BOT (Python)`
++ thefuckpoem
+    + `幹詩一籮筐 (HTML/CSS)`
++ DHT11
+    + `Arduino DHT11溫溼度監控實作 (C/C++)`
